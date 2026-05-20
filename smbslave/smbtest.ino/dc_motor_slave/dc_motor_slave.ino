d521c51cda49b38127c8f11a04fe0fc134fd1fb2
loict <loictruyen@gmail.com>
2026-05-20 12:51:36 +0200

    /

--- smbslave/smbtest.ino/dc_motor_slave/dc_motor_slave.ino
+++ smbslave/smbtest.ino/dc_motor_slave/dc_motor_slave.ino
@@ -2,11 +2,41 @@
 #include "motor_control.h"
 #include "sensors.h"
 
-#define I2C_SLAVE_ADDR 0x07 (0b0111)
+// Eerste 3 bits voor DC-motormodule
+#define DC_MODULE_PREFIX 0b1110000
+
+// DIP-switch pinnen
+#define ADDR_BIT0 PD4
+#define ADDR_BIT1 PD5
+#define ADDR_BIT2 PD6
+#define ADDR_BIT3 PD7
+
+uint8_t readModuleAddress()
+{
+    uint8_t module_id = 0;
+
+    // Lees elke DIP-switch bit uit
+    module_id |= (digitalRead(ADDR_BIT0) << 0);
+    module_id |= (digitalRead(ADDR_BIT1) << 1);
+    module_id |= (digitalRead(ADDR_BIT2) << 2);
+    module_id |= (digitalRead(ADDR_BIT3) << 3);
+
+    // Voeg vaste DC-prefix toe
+    return DC_MODULE_PREFIX | module_id;
+}
 
 void setup() 
 {
-    smb.begin(I2C_SLAVE_ADDR);
+    // DIP-switch pinnen als input
+    pinMode(ADDR_BIT0, INPUT);
+    pinMode(ADDR_BIT1, INPUT);
+    pinMode(ADDR_BIT2, INPUT);
+    pinMode(ADDR_BIT3, INPUT);
+
+    // Volledig I2C-adres bepalen
+    uint8_t slave_address = readModuleAddress();
+
+    smb.begin(slave_address);
 
     motorInit();
     sensorsInit();
@@ -14,10 +44,9 @@ void setup()
 
 
 void loop() {
-    motorUpdate();   // read I2C registers → control motor
+    motorUpdate();
 
-
-    updateSensors(); // write current + rpm back to registers
+    updateSensors();
 
     delay(10); // 100 Hz control loop
 }
